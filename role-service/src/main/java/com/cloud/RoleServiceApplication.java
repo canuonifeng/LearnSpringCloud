9b28942d62911bd58d3d10aee3743dc53df41413
nifeng <nifeng@howzhi.com>
2017-08-27 16:09:19 +0800

    config client

--- role-service/src/main/java/com/cloud/RoleServiceApplication.java
+++ role-service/src/main/java/com/cloud/RoleServiceApplication.java
@@ -1,49 +1,22 @@
 package com.cloud;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.sleuth.Sampler;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.cloud.entity.Role;
 
 @SpringBootApplication
-@RestController
 @EnableDiscoveryClient
 public class RoleServiceApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(RoleServiceApplication.class, args);
 	}
-
-	private static final Logger LOG = Logger.getLogger(RoleServiceApplication.class.getName());
-
-	@RequestMapping("/role")
-	public Role getRole(@RequestParam String code){
-		LOG.log(Level.INFO, "role info is being called");
-		return new Role(code+"管理员", code);
-	}
 	
-	@Value("${from}")
-	private String from;
-	
-	@RequestMapping("/from")
-	public String from() {
-		return from;
-	}
-	
-	@Bean
-	public Sampler getSample() {
-		return new AlwaysSampler();
-	}
+//	@Bean
+//	public Sampler getSample() {
+//		return new AlwaysSampler();
+//	}
 }
---
+++ role-service/src/main/java/com/cloud/controller/RoleController.java
@@ -0,0 +1,33 @@
+package com.cloud.controller;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cloud.RoleServiceApplication;
+import com.cloud.entity.Role;
+
+@RestController
+@RefreshScope
+public class RoleController {
+	private static final Logger LOG = Logger.getLogger(RoleServiceApplication.class.getName());
+
+	@RequestMapping("/role")
+	public Role getRole(@RequestParam String code){
+		LOG.log(Level.INFO, "role info is being called");
+		return new Role(code+"管理员", code);
+	}
+	
+	@Value("${from}")
+	public String from;
+	
+	@RequestMapping("/from")
+	public String from() {
+		return from;
+	}
+}
