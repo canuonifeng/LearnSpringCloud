47418cf5959b90ec129e2b7e582760eb0885d3bf
nifeng <nifeng@howzhi.com>
2017-08-27 18:29:29 +0800

    fix

--- service-turbine/src/main/java/com/cloud/ServiceTurbineApplication.java
+++ service-turbine/src/main/java/com/cloud/ServiceTurbineApplication.java
@@ -1,16 +1,18 @@
 package com.cloud;
 
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableTurbine
+@EnableDiscoveryClient
+@EnableHystrixDashboard
 public class ServiceTurbineApplication {
 
 	public static void main(String[] args) {
-
-			new SpringApplicationBuilder(ServiceTurbineApplication.class).web(true).run(args);
+		SpringApplication.run(ServiceTurbineApplication.class, args);
 	}
 }
